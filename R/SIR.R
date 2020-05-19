c16717eff66d672a0adaa82475b17067824727ec
Tyler Cobian <tylercobian@Tylers-MacBook-Air-4.local>
2020-05-19 15:23:33 -0700

    write up of functions

--- R/SIR.R
+++ R/SIR.R
@@ -1,10 +1,13 @@
 #' SIR model for the spread of COVID-19
 #' 
-#' @param Suceptible = amount of people exposed the the disease
+#' @param s = amount of people exposed the the disease
 #' @param Infected = amoun of people at any one time who has the disease
 #' @param Recovered = amout of people who reover from COVI-19
-#'
+#' @param b = the number of people one infected indiviual will transmit to
+#' @param k = fraction of the infected group who will recover in any given day
 
-
-
+SIR = function(b = 5, ){
+  infected = b*s*i-k*i
+  return(infected)
+}
 
