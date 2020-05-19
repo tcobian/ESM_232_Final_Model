304b79b43c3fa3ef409d7b1cd1195bf006f96608
Tyler Cobian <tylercobian@Tylers-MacBook-Air-4.local>
2020-05-19 15:45:55 -0700

    update functions

--- R/weather.R
+++ R/weather.R
@@ -1,1 +1,8 @@
 #' Weather model that will effect how increases in temperature will effect the effectiveness of stay at home orders and amount of mobil emissions
+#' @param mean_temp = the mean temperature in degrees fasrenheit that week across the state of CA
+#' @return miles_day = the amount of miles drive in a day in the state of CA
+
+
+weather = function(mean_weather, ca_average = 65){
+  miles_day = miles_day * 0.5 * (mean_weather - ca_average)
+}
