4579a46773a9e192ee642135cdae9343f78c282d
Tyler Cobian <tylercobian@Tylers-MacBook-Air-4.local>
2020-05-26 15:28:53 -0700

    Function update

--- R/SIR.R
+++ R/SIR.R
@@ -1,7 +1,7 @@
 #' SIR model for the spread of COVID-19
 #' 
 #' @param s = amount of people exposed the the disease
-#' @param Infected = amoun of people at any one time who has the disease
+#' @param Infected = amount of people at any one time who has the disease
 #' @param Recovered = amout of people who reover from COVI-19
 #' @param b = the number of people one infected indiviual will transmit to
 #' @param k = fraction of the infected group who will recover in any given day
